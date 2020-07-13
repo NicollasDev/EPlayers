c34dee9729d4e39c52482f72a1abcd6f0fb756c1
NicollasDileo06 <nicgato2013@gmail.com>
2020-07-13 15:24:43 -0300

    Primeiro commit

---
+++ Interfaces/IEquipe.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using EPlayers.Models;
+
+namespace EPlayers.Interfaces
+{
+    public interface IEquipe
+    {
+         void Create(Equipe e);
+         List<Equipe> ReadAll();
+         void Update(Equipe e);
+         void Delete(int IdEquipe);
+    }
+}
